ef3b79d7cc262d00414fc34a832288101e67f957
Jakub Maciocha <ciamcio@dellg15jm.home>
2024-04-07 14:12:46 +0200

    Enum class implemnting const for menu class

---
+++ Menu/MenuConsts.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class MenuConsts(Enum):
+    # Bounderies for simulation main menu 
+    mainMenuLowerBounderie = 1
+    mainMenuHigherBounderie = 4
+    # Options of simulation main menu
+    simulationSettings = 1
+    printChart = 2
+    printResults = 3
+    saveToFile = 4
+    
